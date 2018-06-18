549444924d0bb949bbd06a9e42ec145d250a5731
kanji731 <nagao@NagaonoMacBook-puro.local>
2018-06-18 09:34:39 +0900

    Edit javascripts user-index

--- app/assets/javascripts/user-index.js
+++ app/assets/javascripts/user-index.js
@@ -1,14 +1,29 @@
 $(function () {
+  function appendName(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id ="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`;
+    $('#user-search-result').append(html);
+  }
+  function formClear() {
+    $("#user-search-result").empty();
+  }
   $('#user-search-field').on("keyup", function () {
-    var input = $("#user-search-field").val();
+    formClear();
+    var input = $(this).val();
     $.ajax({
       type: 'GET',
       url: '/users',
-      data: { keyword: input },
+      data: { search_name: input },
       dataType: 'json'
     })
       .done(function (users) {
-
+        if (users.length !== 0) {
+          users.forEach(function (user) {
+            appendName(user);
+          });
+        }
       })
       .fail(function () {
 
