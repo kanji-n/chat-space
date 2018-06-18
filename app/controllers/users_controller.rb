84ec74b149d18609c83d72d15983c8bba71dbf2e
kanji731 <nagao@NagaonoMacBook-puro.local>
2018-06-18 09:43:25 +0900

    Edit controllers users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:user]}%").limit(100)
+    @users = User.where('name LIKE(?)', "%#{params[:search_name]}%").limit(100)
     respond_to do |format|
       format.html
       format.json
