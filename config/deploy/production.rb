959feec72f25da62d1926c0623a95fff4ed5d96c
kanji731 <nagao@NagaonoBookpuro.lan>
2018-06-21 02:34:44 +0900

    Edit config deploy production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server '18.179.145.35', user: 'ec2-user', roles: %w{app db web}
+
 
 
 # role-based syntax
